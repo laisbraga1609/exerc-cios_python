214e613ad37ab929e1af8a63d9f7e428d240b3b3
Laís Braga de Oliveira <laisgame1609@gmail.com>
2026-02-12 10:10:21 -0300

    manipulação_texto

---
+++ analisador-texto.py
@@ -0,0 +1,6 @@
+nome= str(input('digite seu nome completo: ')).strip()
+print('Analisando o seu nome...')
+print('seu nome em maiscula é {}'.format(nome.upper()))
+print('seu nome em minuscula é {}'.format(nome.lower()))
+print('seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+print('seu primeiro nome tem {} letras'.format(nome.find(' ')))
